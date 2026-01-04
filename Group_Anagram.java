bc434e910d4e06e781ff6df951a6cf458dcb0860
Ramakrishna7792 <pvramakrishna922@gmail.com>
2026-01-04 22:11:45 +0530

    Group Anagram Java Solution

---
+++ Group_Anagram.java
@@ -0,0 +1,16 @@
+class Solution 
+{
+    public List<List<String>> groupAnagrams(String[] strs) 
+    {
+        Map<String,List<String>> mpp = new HashMap<>();
+        for(String s:strs)
+        {
+            char[] ch=s.toCharArray();
+            Arrays.sort(ch);
+            String key=new String(ch);
+            mpp.putIfAbsent(key,new ArrayList<>());
+            mpp.get(key).add(s);
+        }
+        return new ArrayList<>(mpp.values());
+    }
+}
